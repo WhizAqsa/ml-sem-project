a33c72dc6955046bf92d67ec42306fb71f4e4026
aqsa <aqsabatool2005@gmail.com>
2024-12-19 20:42:29 +0500

    feat: delete main.py, bagging,rf,svm code done

--- run_svm_model.py
+++ run_svm_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from src.utils import fit_and_get_predictions,separate_features_label, get_best_params_grid_search
 from src.preprocessing import smote, remove_outliers
 from scripts.preprocess import preprocess_data
-from xgboost import XGBClassifier
+from sklearn.svm import SVC
 from src.evaluation import get_eval_metrics
 from src.plots import visualize_confusion_matrix
 from sklearn.preprocessing import StandardScaler
@@ -11,25 +11,7 @@ from zipfile import ZipFile
 import sys
 
 
-"""
-1. Load all the dataset
-    - you noob
-    - merge rice and cotton of all three years
-2. Create the 3 splits
-    i.e. 21,22 and 23
-         22,23 and 21
-         21,23 and 22
-3. Run default model on the 3 splits.
-4. Do grid search on the whole dataset.
-5. Perform cross validation.
-
-all results should be properly logged.
-all the plots should either be saved in a directory or be shown at runtime
-all the results should also be logged to a text file where you can then show it to sir
-"""
-
-
-def run_xgb_model():
+def run_svm_model():
     # unzip the data file
     file_name = "data/raw/Crop-dataset.zip"
     with ZipFile(file_name, 'r') as zip:
@@ -113,70 +95,51 @@ def run_xgb_model():
     y_encoded_22 = encoder.transform(y_resampled_22)
     y_encoded_23 = encoder.transform(y_resampled_23)
     
-    # save the preprocessed data
-    print("Saving the preprocessed data...")
-
-    # Combine and add labels for 2021-22
-    combined_df_2021_22 = pd.concat([pd.DataFrame(X_train_scaled_2021_22), pd.DataFrame(X_test_scaled_2021_22)], ignore_index=True)
-    combined_labels_2021_22 = pd.concat([pd.Series(y_train_encoded_2021_22), pd.Series(y_test_encoded_2021_22)], ignore_index=True)
-    combined_df_2021_22['label'] = combined_labels_2021_22
-
-    # Combine and add labels for 2021-23
-    combined_df_2021_23 = pd.concat([pd.DataFrame(X_train_scaled_2021_23), pd.DataFrame(X_test_scaled_2021_23)], ignore_index=True)
-    combined_labels_2021_23 = pd.concat([pd.Series(y_train_encoded_2021_23), pd.Series(y_test_encoded_2021_23)], ignore_index=True)
-    combined_df_2021_23['label'] = combined_labels_2021_23
-
-    # Combine and add labels for 2022-23
-    combined_df_2022_23 = pd.concat([pd.DataFrame(X_train_scaled_2022_23), pd.DataFrame(X_test_scaled_2022_23)], ignore_index=True)
-    combined_df_2021_22.to_csv('data/processed/cotton_rice_2021_22.csv', index=False)
-    combined_df_2021_23.to_csv('data/processed/cotton_rice_2021_23.csv', index=False)
-    combined_df_2022_23.to_csv('data/processed/cotton_rice_2022_23.csv', index=False)
-      
     # run the default 
-    print("Running default xgb model...")
-    model = XGBClassifier(random_state=42)
-    y_pred_xgb = fit_and_get_predictions(model, X_train_scaled_2021_22, y_train_encoded_2021_22, X_scaled_23)
+    print("Running default SVM model...")
+    model = SVC(random_state=42)
+    y_pred_rf = fit_and_get_predictions(model, X_train_scaled_2021_22, y_train_encoded_2021_22, X_scaled_23)
     
     # print the classification report
-    score, eval_report, cm = get_eval_metrics(y_encoded_23, y_pred_xgb)
-    print(f"Accuracy score for default xgb model: {score}")
-    print(f"Classification report for default xgb model:\n{eval_report}")
+    score, eval_report, cm = get_eval_metrics(y_encoded_23, y_pred_rf)
+    print(f"Accuracy score for default SVM model: {score}")
+    print(f"Classification report for default SVM model:\n{eval_report}")
     
     # visualise the conf matrix
     print("Visualise the confusion matrix...")
     visualize_confusion_matrix(cm)
     
     # grid search
-    print("XGBoost Grid Search")
-    params_grid_xgboost = {
-    "max_depth": [3 ,5, 7],
-    "learning_rate": [0.01, 0.05, 0.1, 0.3],
-    "n_estimators": [100, 200, 300]
+    print("SVM Grid Search")
+    params_grid_svm = {
+    'C': [0.001, 0.01, 0.1, 1, 10],
+    'gamma': [0.001, 0.01, 0.1, 0.5, 1],
+    'kernel': ['rbf', 'linear', 'poly', 'sigmoid']
     }
-    best_params_xgb = get_best_params_grid_search(
-    XGBClassifier(random_state=42,verbosity=2),
+    best_params_svm = get_best_params_grid_search(
+    SVC(random_state=42),
     X_train_scaled_2021_22,
     y_train_encoded_2021_22,
-    params_grid_xgboost
+    params_grid_svm
     )
-    print(f"XGBoost Best params: {best_params_xgb}")
-    best_xgb_model = XGBClassifier(
+    print(f"SVM Best params: {best_params_svm}")
+    best_svm_model = SVC(
     random_state=42,
-    **best_params_xgb
+    **best_params_svm
     )
     
-    print("Running the best xgb model...")
-    y_pred_xgb_best = fit_and_get_predictions(
-    best_xgb_model,
+    print("Running the best SVM model...")
+    y_pred_svm_best = fit_and_get_predictions(
+    best_svm_model,
     X_train_scaled_2021_22, 
     y_train_encoded_2021_22,
     X_scaled_23
     )
     
     # print the classification report for the best model
-    score, eval_report, cm = get_eval_metrics(y_encoded_23, y_pred_xgb_best)
-    print(f"Accuracy score for the best xgb model: {score}")
-    print(f"Classification report for the best xgb model:\n{eval_report}")
+    score, eval_report, cm = get_eval_metrics(y_encoded_23, y_pred_svm_best)
+    print(f"Accuracy score for the best SVM model: {score}")
+    print(f"Classification report for the best SVM model:\n{eval_report}")
     
     # visualise the conf matrix
     print("Visualise the confusion matrix after applying grid search...")
@@ -184,4 +147,4 @@ def run_xgb_model():
     
 
 if __name__ == "__main__":
-    run_xgb_model()
+    run_svm_model()
